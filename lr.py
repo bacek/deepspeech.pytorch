4b50850b4ac7f4692e170e4477eb917ba425e290
Vasily Chekalkin <bacek@bacek.com>
2018-08-22 13:23:28 +1000

    Fix 1cycle

--- lr.py
+++ lr.py
@@ -5,7 +5,7 @@ def linear_decay(frm, to, steps):
     '''
     Generate linear values between fmr and to for number of steps.
     '''
-    pct = (to-frm)/(steps-1)
+    pct = (to-frm)/steps
     step = 0
     while step < steps:
         val = frm + pct * step
@@ -55,21 +55,23 @@ class SGDSchedule(object):
 class OneCycle(SGDSchedule):
     def __init__(self, optimizer, epochs, lr_from, lr_to, momentum_from, momentum_to, anneal_pct, anneal_rate):
         # Total number of train epochs
-        cycle_len = int(epochs * (1 - anneal_pct))
+        cycle_len = int(epochs * (1 - anneal_pct) / 2)
 
         lr = itertools.chain.from_iterable([
-            linear_decay(lr_from, lr_to, cycle_len / 2),
-            linear_decay(lr_to, lr_from, cycle_len / 2),
-            div_decay(lr_from, anneal_rate, epochs - cycle_len)
+            linear_decay(lr_from, lr_to, cycle_len),
+            linear_decay(lr_to, lr_from, cycle_len),
+            div_decay(lr_from, anneal_rate, epochs - 2 * cycle_len)
             ])
 
         momentum = itertools.chain.from_iterable([
-            linear_decay(momentum_from, momentum_to, cycle_len / 2),
-            linear_decay(momentum_to, momentum_from, cycle_len / 2),
+            linear_decay(momentum_from, momentum_to, cycle_len),
+            linear_decay(momentum_to, momentum_from, cycle_len),
             [momentum_from for i in range(cycle_len, epochs)]
             ])
 
         super().__init__(logging.getLogger('lr.OneCycle'), optimizer, lr, momentum)
+
+        self.logger.info("lr: {}->{} m: {}->{}".format(lr_from, lr_to, momentum_from, momentum_to))
 
 class Anneal(SGDSchedule):
     def __init__(self, optimizer, epochs, lr, learning_anneal):
